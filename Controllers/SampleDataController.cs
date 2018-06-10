ea6afbe39d9bd43dedde2d73ddbfc25d79439a27
Falkeye <micke.net@gmail.com>
2018-06-10 11:49:09 +0200

    Testing

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace aspnetcore_angular_sample.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Testing"
         };
 
         [HttpGet("[action]")]
